23ea7b8f46bdb80874bb10d0b0a1524932162bed
jooys130 <wndustn130@gmail.com>
2020-06-22 08:13:06 -0700

    add bookmark.php

---
+++ CafeFinder_aroundERICA/bookmark.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>
+<?php session_start(); ?>
+
+<html>
+   <head>
+      <title>BookMark</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+      <link rel="stylesheet" href="assets/css/main.css" />
+      <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+   </head>
+   <body class="is-loading"></body>
+
+      <!-- Wrapper -->
+         <div id="wrapper">
+
+            <!-- Header -->
+               <header id="header">
+						<a href="index.php" class="logo">Cafe Finder</a>
+					</header>
+
+            <!-- Nav -->
+               <nav id="nav">
+                  <ul class="links">
+                     <li><a href="cafe.php">CAFE</a></li>
+                     <li><a href="intro.html">INTRODUCE</a></li>
+                     <li class="active"><a href="bookmark.php">BOOKMARK</a></li>
+                  </ul>
+               </nav>
+
+            <!-- Main -->
+               <div id="main">
+
+                  <!-- Post -->
+                     <section class="post">
+                        <header class="major">
+                           <h2>BookMark</h2>
+                        </header>
+                     </section>
+                     <section>
+                        List
+                    </section>
+               </div>
+
+            <!-- Copyright -->
+               <div id="copyright">
+                  <ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">디자인</a></li></ul>
+               </div>
+
+         </div>
+
+      <!-- Scripts -->
+         <script src="assets/js/jquery.min.js"></script>
+         <script src="assets/js/jquery.scrollex.min.js"></script>
+         <script src="assets/js/jquery.scrolly.min.js"></script>
+         <script src="assets/js/skel.min.js"></script>
+         <script src="assets/js/util.js"></script>
+         <script src="assets/js/main.js"></script>
+
+   </body>
+</html>
